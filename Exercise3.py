6b0ea5728857faca8aa88ba358cb3461261a75b6
Rayce <erayce666@gmail.com>
2024-03-26 21:03:20 -0400

    Commit Exercise 3

--- Exercise3.py
+++ Exercise3.py
@@ -3,7 +3,7 @@ Write a program that asks the user to enter the width and length of a room. Once
 the values have been read, your program should compute and display the area of the
 room. The length and the width will be entered as floating point numbers. Include
 units in your prompt and output message; either feet or meters, depending on which
-unit you are more comfortable working with.
+unit you are more comfortable working with
 '''
 
 length = float(input('Enter the length of the rectangle in meters. '))
