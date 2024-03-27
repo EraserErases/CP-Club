24c307ee41767f8570f197eb3cf03be74e841d73
Rayce <erayce666@gmail.com>
2024-03-26 21:12:16 -0400

    Commit Exercise 4

---
+++ Exercise4.py
@@ -0,0 +1,11 @@
+"""
+Create a program that reads the length and width of a farmer's field from the user in
+feet. Display the area of the field in acres.
+"""
+
+length = float(input('Length of field in feet. '))
+width = float(input('Length of width in feet. '))
+
+area = (length * width) / 43560
+
+print(f'With the inputed length {length} feet, and width {width}, the area of the field in acres is {area}. ')
